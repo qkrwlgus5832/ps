b32e255483d84449f4d28d1a560ce3eb03b187c1
박지현 <pjh_5890@naver.com>
2020-07-27 20:38:47 +0900

    중앙값 구하기

---
+++ boj/중앙값 구하기/중앙값 구하기.cpp
@@ -0,0 +1,66 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <iostream>
+#include <queue>
+#include <vector>
+#include <functional>
+#include <algorithm>
+
+using namespace std;
+
+void solution(vector <int> &result, vector <int> &input) {
+	priority_queue <int> maxHeap;
+	priority_queue <int, vector <int>, greater<int>> minHeap;
+	int mid = input[0];
+	result.push_back(input[0]);
+
+	for (int i = 1; i < input.size(); i++) {
+		if (input[i] > mid) 
+			minHeap.push(input[i]);
+
+		else
+			maxHeap.push(input[i]);
+		
+		if (i % 2 == 0) {
+			if (minHeap.size() < maxHeap.size()) {
+				minHeap.push(mid);
+				mid = maxHeap.top();
+				maxHeap.pop();
+			}
+			else if (minHeap.size() > maxHeap.size()) {
+				maxHeap.push(mid);
+				mid = minHeap.top();
+				minHeap.pop();
+			}
+			result.push_back(mid);
+		}
+
+
+	}
+}
+int main() {
+	int T;
+	cin >> T;
+	for (int t = 0; t < T; t++) {
+		int m;
+		scanf("%d", &m);
+	
+		vector <int> result;
+		vector <int> input;
+		
+		input.resize(m);
+		for (int i = 0; i < m; i++) {
+			scanf("%d", &input[i]);
+		}
+		
+		solution(result, input);
+		
+		printf("%d\n", result.size());
+		for (int value : result) {
+			printf("%d ", value);
+		}
+		printf("\n");
+		
+	}
+	//system("Pause");
+}
--- boj/체스/체스.cpp
+++ boj/체스/체스.cpp
@@ -1,23 +1,90 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <iostream>
+#include <vector>
 using namespace std;
 
 
-bool chess[1001][1001];
+int chess[1001][1001];
+int n, m;
+
+void input(vector <pair <int, int>> &v) {
+	int count;
+	cin >> count;
+
+	int x, y;
+	for (int i = 0; i < count; i++) {
+		scanf("%d %d", &x, &y);
+		v.push_back({ x,y });
+		chess[x][y] = 2;
+	}
+}
+
+
+void queenCheck(vector <pair <int, int>> &queen) {
+	int dir[8][2] = { {-1,0}, {1,0}, {0,-1}, {0,1},{1, -1}, {-1,1}, {-1,-1}, {1,1} };
+	for (auto location : queen) {
+		for (int i = 0; i < 8; i++) {
+			int length = 1;
+			while (1) {
+				int ni = location.first + dir[i][0] * length;
+				int nj = location.second + dir[i][1] * length;
+
+				if (ni < 1 || nj < 1 || ni > n || nj > m || chess[ni][nj] == 2)
+					break;
+
+				chess[ni][nj] = 1;
+				length++;
+			}
+		}
+	}
+}
+
+void knightCheck(vector <pair <int, int>> &knight) {
+	int dir[8][2] = { {-1, -2}, {-2,-1}, {-2, 1}, {-1, 2}, {1, -2}, {2, -1}, {2,1}, {1,2} };
+
+	for (auto location : knight) {
+		for (int i = 0; i < 8; i++) {
+			int ni = location.first + dir[i][0];
+			int nj = location.second + dir[i][1];
+
+			if (ni < 1 || nj <1 || ni> n || nj >m)
+				continue;
+
+			chess[ni][nj] = 1;
+		}
+	}
+}
+int solution(const vector <vector <pair <int ,int>>>  &v) {
+	vector <pair <int, int>> queen = v[0];
+	vector <pair <int, int>> knight = v[1];
+
+	queenCheck(queen);
+	knightCheck(knight);
+
+	int result = 0;
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= m; j++) {
+			if (chess[i][j] == 0)
+				result++;
+		}
+	}
+	return result;
+}
 
 int main() {
-	int n, m;
+
 	cin >> n >> m;
 
 	int q;
-	cin >> q;
+	vector <pair <int, int>> queen;
+	input(queen);
+	vector <pair <int, int>> knight;
+	input(knight);
+	vector <pair <int, int>> pawn;
+	input(pawn);
 
-	int x, y;
-	for (int i = 0; i < q; i++) {
-		scanf("%d %d", &x, &y);
-		chess[x][y] = true;
+	cout << solution({ queen, knight, pawn });
 
-	}
-	system("Pause");
+	//system("Pause");
 }
